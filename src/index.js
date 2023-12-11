3dfc11fef253744a7992322479c574f763638473
Nate Watkins <143032710+nw-nine@users.noreply.github.com>
2023-12-11 14:10:34 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1140d7187a1f40ef8851cf05d679d74d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
